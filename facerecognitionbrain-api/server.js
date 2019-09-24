84e81942186ad1cfb11fe5b9baeac31a13f8b345
Willian <willianwelbert@yahoo.com.br>
2019-09-24 20:58:43 -0300

    implements controlers, simplifying server.js, moves API (clarifai) call to the backend

--- facerecognitionbrain-api/server.js
+++ facerecognitionbrain-api/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const register = require('./controlers/register');
+const signin = require('./controlers/signin');
+const profile = require('./controlers/profile');
+const image = require('./controlers/image');
+
 const db = require('knex')({
   client: 'pg',
   connection: {
@@ -41,78 +46,21 @@ app.get('/', (req, res) => {
   res.send(database.users)
 })
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where( 'email', '=', req.body.email)
-    .then( data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if(isValid){
-        return db.select('*')
-          .from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else{
-        res.status(400).json('wrong credentials');
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-});
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) } );
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-
-  db.transaction(trx => {
-    trx.insert({hash, email})
-    .into('login')
-    .returning('email')
-    .then( loginEmail => {
-      return trx('users')
-      .returning('*')
-      .insert({
-        email: loginEmail[0],
-        name: name,
-        joined: new Date()
-      })
-      .then(user => {
-        res.json(user[0]);  
-      })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  .catch( err => {
-    res.status(400).json('unable to register')
-    })
-  
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
 
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id}).then( user => {
-    if (user.length) {res.json(user[0])}
-    else { res.status(400).json('not found') }
-  }).catch( err => res.status(400).json('not found'))
-  
-} )
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)} )
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0])
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-} )
+app.put('/image', (req, res) => { image.handleImage(req, res, db) } )
+
+app.post('/imageurl', (req, res) => { image.handleAPICall(req, res) } )
 
 
 
 app.listen(3001, () => {
   console.log('app is running on port 3001');
 })
+
+console.log(process.env)
